6f83992236a6c777999ca8edcd50d427d16c397d
Yazra4243 <228a1a4243@risekrishnasaiprakasam.edu.in>
2024-10-23 10:24:32 +0530

    Update java 1.java

--- akash/java 1.java
+++ akash/java 1.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
 public static void main(String args[])
 {
-System.out.println("Hello World"); //Prints on screen “Hello World”
+System.out.println("Hello"); //Prints on screen “Hello World"
 }
 }
